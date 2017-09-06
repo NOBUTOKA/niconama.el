ac5fd9b5310222c083e49fc201eab12a1fe1f917
Nobuto <nobutoka@gmail.com>
2017-09-06 21:31:19 +0900

    微修正

--- niconama.el
+++ niconama.el
@@ -33,6 +33,7 @@
 
 ;; And then, type M-x niconama-comment-viewer to activate comment viewer.
 ;; C-RET in "Write Comment" buffer submit the contents of this buffer to broadcast.
+;; If you want to switch 184 attribute of comment, use C-c C-y.
 
 ;; To kill the comment viewer, use M-x niconama-kill-comment-viewer and type process number
 ;; shown in comment viewer buffer name as <process number>: <broadcast title>.
